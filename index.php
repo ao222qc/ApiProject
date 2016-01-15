bc2696a60dc22dbdc3aebbfe01d5298d88669d3e
kevin <kn222gn@student.lnu.se>
2016-01-15 20:07:36 +0100

    Moving requires to init.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-
 error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+require_once("init.php");
 
-require_once("Api/api.php");
-
-$api = new Api();
-
-$api->CreateCollection();
+Template::Render();
---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set('display_errors', 'On');
+
+require_once("Api/api.php");
+require_once("template.php");
+Template::Init();
+
+$api = new Api();
