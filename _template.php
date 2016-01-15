6e241066550488e09ba22977e55150eb0acff31c
Anton <anton_peace@hotmail.com>
2016-01-15 22:17:25 +0100

    adding link to upload.php

--- _template.php
+++ _template.php
@@ -5,6 +5,7 @@
     <body>
         <h1>Api tester</h1>
         <a href="collection.php">Find collection</a>
+        <a href="upload.php">Upload artifact</a>
         <hr>
         <?= Template::$Body ?>
     </body>
