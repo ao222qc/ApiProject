37b87e5ba5c4ac8133855f157ce4047add593ef6
ao222qc <ao222qc@student.lnu.se>
2016-01-15 20:26:14 +0100

    hr

--- index.php
+++ index.php
@@ -2,11 +2,3 @@
 error_reporting(E_ALL);
 require_once("init.php");
 
-<<<<<<< HEAD
-Template::Render();
-=======
-//$api->CreateCollection();
-
-//$file = $api->GetCollection("9ab74b");
-
->>>>>>> origin/Adam
