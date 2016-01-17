3e4af82fb2ce54baf9d730cb01b65f47ed71f1cf
ao222qc <ao222qc@student.lnu.se>
2016-01-17 15:42:55 +0100

    adding update function

---
+++ update.php
@@ -0,0 +1,52 @@
+<?php
+error_reporting(E_ALL);
+require_once("init.php");
+
+
+
+$itemID = null;
+$collectionID = null;
+$extension = null;
+
+if(isset($_GET["id"]) && isset($_GET["cid"]))
+{
+    $itemID = $_GET["id"];
+    $collectionID = $_GET["cid"];
+
+    $originalCollection = $api->GetCollection($collectionID);
+
+    if($originalCollection != NULL)
+    {
+        $item = $originalCollection->GetArtifact($itemID);
+
+        if($item != null)
+        {
+            $extension = $item->GetExtension();
+
+            if(isset($_FILES["file"]))
+            {
+                $item->Update($_FILES["file"]);
+            }
+        }
+    }
+}
+
+
+
+echo '
+     <br>
+     <form enctype="multipart/form-data" action="" method="POST">
+        Artifact <b>'.$itemID.'</b> from collection <b><a href="find.php?id='.$collectionID.'">'.$collectionID.'</a></b>
+        <br>
+        <br>
+        <input type="file" name="file" accept=".'.$extension.'">
+        <button>Update</button>
+    </form>
+';
+
+
+
+
+
+
+Template::Render();
