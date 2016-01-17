658ff39ef07286abf662b0b2901bc8d01678fbf9
ao222qc <ao222qc@student.lnu.se>
2016-01-17 18:46:31 +0100

    adding a way to check if an artifact is a copy, in this case we'll fetch the data again upon getting the collection

--- Api/Collection.php
+++ Api/Collection.php
@@ -6,15 +6,21 @@ class Collection
     private $name;
     private $list = array();
 
+    public $IsSnapshot = false;
+
     public function __construct($Name)
     {
         $this->ID = Helper::GenerateID();
         $this->name = $Name;
-
         $this->save();
     }
     //On unserialize
     public function __wakeup()
+    {
+        $this->Update();
+    }
+
+    private function Update()
     {
         foreach ($this->list as $item)
         {
@@ -23,6 +29,17 @@ class Collection
                 if ($item->IsGhost())
                 {
                     unset($this->list[$item->GetID()]);
+                }
+            }
+            elseif ($item instanceof Collection)
+            {
+                if ($item->IsGhost())
+                {
+                    unset($this->list[$item->GetID()]);
+                }
+                else
+                {
+                    $this->list[$item->GetID()]->IsSnapshot = true;
                 }
             }
         }
@@ -58,14 +75,66 @@ class Collection
             return $this->list[$artifactID];
         }
         return null;
-
     }
 
-    public function Delete($id)
+    public function GetCollection($id)
     {
-        $this->list[$id]->Delete();
+        if (isset($this->list[$id]))
+        {
+            if ($this->list[$id]->IsSnapshot)
+            {
+                $this->list[$id] = Api::GetCollection($id);
+            }
+
+            return $this->list[$id];
+        }
+
+        return null;
+    }
+
+    public function Suicide()
+    {
+        if ($this->IsSnapshot)
+        {
+            $tmp = Api::GetCollection(Api::COLLECTIONPATH.$this->GetID());
+            $this->list = $tmp->GetList();
+            $this->IsSnapshot = false;
+        }
+
+        foreach ($this->list as $item)
+        {
+            if ($item instanceof Artifact)
+            {
+                $this->list[$item->GetID()]->Suicide();
+                unset($this->list[$item->GetID()]);
+            }
+            elseif ($item instanceof Collection)
+            {
+                $item->Suicide();
+            }
+        }
+        unlink(Api::COLLECTIONPATH.$this->GetID());
+    }
+
+    public function DeleteArtifact($id)
+    {
+        $this->list[$id]->Suicide();
         unset($this->list[$id]);
 
+        $this->save();
+    }
+
+    public function DeleteCollection($id)
+    {
+        $this->GetCollection($id)->Suicide();
+        unset($this->list[$id]);
+
+        $this->save();
+    }
+
+    public function Remove($id)
+    {
+        unset($this->list[$id]);
         $this->save();
     }
 
@@ -79,4 +148,8 @@ class Collection
         file_put_contents(Api::COLLECTIONPATH . $this->GetID(), serialize($this));
     }
 
+    public function IsGhost()
+    {
+        return !file_exists(Api::COLLECTIONPATH.$this->GetID());
+    }
 }
